908c3090b8ca3af776d5567761100c00d3ff4761
hadley <h.wickham@gmail.com>
2011-11-08 11:17:49 -0600

    Don't load aplpack to avoid tcl/tk

--- code-data/bagplots.R
+++ code-data/bagplots.R
@@ -1,6 +1,5 @@
 # Bag plots ------------------------------------------------------------------
 
-library(aplpack)
 library(RColorBrewer)
 cols <- c("white", brewer.pal(9, "Blues"), "black")
 
@@ -8,7 +7,7 @@ census <- read.csv("counties.csv")
 
 pdf("../images/counties-bag.pdf")
 par(mar=c(4.1, 4.1, 1, 1))
-with(census, bagplot(Latitude/10000, JanTmp/10, xlab="Latitude (in degrees)", ylab="Temperatures in January (in F)"))
+with(census, aplpack::bagplot(Latitude/10000, JanTmp/10, xlab="Latitude (in degrees)", ylab="Temperatures in January (in F)"))
 dev.off()
 
 pdf("../images/counties-lvbag.pdf")
