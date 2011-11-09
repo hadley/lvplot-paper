a3de6ad84dcc5af510953e71fe5e5ba9e3f8ea5e
hadley <h.wickham@gmail.com>
2011-11-09 11:52:18 -0600

    Recreate figure of letter value errors

---
+++ code-data/letter-val-errors.r
@@ -0,0 +1,23 @@
+library(reshape2)
+library(ggplot2)
+library(stringr)
+
+err_raw <- read.csv("letter-val-errors.csv")
+err <- melt(err_raw, id = c("name", "i"))
+err$sigma <- as.numeric(str_replace(err$variable, "X", ""))
+err$variable <- NULL
+
+
+breaks <- 10^(2:7)
+labels <- format(breaks, sci = F, trim = T)
+
+qplot(i, value, data = err, geom = "line", linetype = factor(sigma)) +
+  geom_text(aes(label = sigma, x = i + 0.25), data = subset(err, i == 20),
+    hjust = 0) + 
+  scale_y_log10(breaks = breaks, labels = labels) + 
+  theme_bw() + 
+  labs(x = "Letter value", y = "Number of observations") +
+  opts(legend.position = "none") +
+  xlim(1, 21)
+  
+ggsave("../images/letter-val-errors.pdf", width = 8, height = 6)
