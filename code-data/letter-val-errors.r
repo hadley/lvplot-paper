0e1258ae4c1ab6f6cc89abbe9a503374ddbe4cdc
hadley <h.wickham@gmail.com>
2011-11-10 08:06:35 -0600

    Improvements to figure 7

--- code-data/letter-val-errors.r
+++ code-data/letter-val-errors.r
@@ -14,10 +14,11 @@ labels <- format(breaks, sci = F, trim = T)
 qplot(i, value, data = err, geom = "line", linetype = factor(sigma)) +
   geom_text(aes(label = sigma, x = i + 0.25), data = subset(err, i == 20),
     hjust = 0) + 
+  geom_point() + 
   scale_y_log10(breaks = breaks, labels = labels) + 
   theme_bw() + 
   labs(x = "Letter value", y = "Number of observations") +
   opts(legend.position = "none") +
   xlim(1, 21)
   
-ggsave("../images/letter-val-errors.pdf", width = 8, height = 6)
+ggsave("../images/letter-val-errors.pdf", width = 6, height = 4)
