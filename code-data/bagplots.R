4cb8d2ca05646a6a3764b5c856ec580d3e11cbe1
Heike Hofmann <hofmann@iastate.edu>
2012-07-22 14:08:18 -0500

    not sure what I changed

--- code-data/bagplots.R
+++ code-data/bagplots.R
@@ -12,5 +12,13 @@ dev.off()
 
 pdf("../images/counties-lvbag.pdf")
 par(mar=c(4.1, 4.1, 1, 1))
-with(census, LVbagplot(Latitude/10000, JanTmp/10, xlab="Latitude (in degrees)", ylab="Temperatures in January (in F)", col=rev(cols)[-(1:2)]))
+foo <- with(census, LVbagplot(Latitude/10000, JanTmp/10, xlab="Latitude (in degrees)", ylab="Temperatures in January (in F)", col=rev(cols)[-(1:2)]))
 dev.off()
+
+
+# outlier identification
+sub <- subset(data.frame(foo$outliers), (x > 35) & (y > 35))
+points(sub$x, sub$y, col=2)
+
+with(census, plot(-Longitude/10000, Latitude/10000))
+points()
